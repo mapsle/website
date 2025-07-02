2f03328556536a16b30d6009199f293ed9ba1a2e
Ingo Wolf <me@ingowolf.au>
2025-07-02 16:01:41 +1000

    add not found page :)

---
+++ app/not-found.tsx
@@ -0,0 +1,21 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="flex p-10 flex-col items-center justify-center gap-3">
+      <h1 className="text-4xl font-bold text-center">404</h1>
+      <p className="text-xl text-center">
+        Looks like you're lost. I can make you a map :)
+      </p>
+      <div className="flex flex-row gap-3">
+        <Button>
+          <Link href="/commission">Commission Me</Link>
+        </Button>
+        <Button variant="ghost">
+          <Link href="/">Home</Link>
+        </Button>
+      </div>
+    </div>
+  );
+}
