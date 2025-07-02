4948931f41a2388e43059d00cb8d38492132c82f
Ingo Wolf <me@ingowolf.au>
2025-07-02 22:22:41 +1000

    add hero map component and move it to there

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,11 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import HeroMap1 from "@/assets/hero-map-1.svg";
-import Image from "next/image";
+import HeroMap1 from "@/assets/hero-map-1";
 
 export default function Home() {
   return (
     <div className="absolute top-0 h-screen -z-10 w-full">
-      <Image
-        src={HeroMap1}
-        alt="Hero Map"
-        className="top-0 left-0 h-full absolute w-full -z-20 object-cover object-left opacity-50"
-      />
+      <HeroMap1 className="h-full absolute top-0 left-0 w-fit" />
       <div className="container mx-auto flex flex-col items-center justify-center h-full gap-5">
         <h1 className="text-4xl text-center">
           One-of-a-kind, hand-drawn maps crafted just for you <br />
