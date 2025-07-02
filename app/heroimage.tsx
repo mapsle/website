80d2d92ab3276d753a1675e2a1cc13c664737386
Ingo Wolf <me@ingowolf.au>
2025-07-02 23:09:49 +1000

    fade in image

--- app/heroimage.tsx
+++ app/heroimage.tsx
@@ -5,10 +5,16 @@ import Image from "next/image";
 
 export default function HeroImage() {
   return (
-    <Image
-      src={HeroGraphic}
-      alt="Hero Graphic"
-      className="w-fit object-cover h-full -z-20 absolute top-0 left-0 opacity-50"
-    />
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1, delay: 0.5 }}
+    >
+      <Image
+        src={HeroGraphic}
+        alt="Hero Graphic"
+        className="w-fit object-cover h-full -z-20 absolute top-0 left-0 opacity-50"
+      />
+    </motion.div>
   );
 }
