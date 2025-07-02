c39ab5b8eeee24cdd2b27db29bbcde945c1c659a
Ingo Wolf <me@ingowolf.au>
2025-07-02 18:26:29 +1000

    start working on commission page

--- app/commission/page.tsx
+++ app/commission/page.tsx
@@ -1,7 +1,12 @@
 export default function CommissionPage() {
   return (
-    <div>
-      <h1>Commission</h1>
+    <div className="absolute top-0 -z-10 w-full h-screen flex md:flex-row flex-col">
+      <div className="w-full flex justify-center items-center">
+        <div className="w-96 h-72 bg-gray-300 rounded"></div>
+      </div>
+      <div className="w-full flex justify-center items-center">
+        <div className="p-3 gap-3 flex flex-col border min-w-96">test</div>
+      </div>
     </div>
   );
 }
