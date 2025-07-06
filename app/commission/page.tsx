6f3c513a0d0d0f1958173793a1d4b896e700ce49
Ingo Wolf <me@ingowolf.au>
2025-07-06 17:00:05 +1000

    add some nice animations

--- app/commission/page.tsx
+++ app/commission/page.tsx
@@ -3,7 +3,7 @@
 import { Button } from "@/components/ui/button";
 import clsx from "clsx";
 import { useMemo, useState } from "react";
-import { motion } from "motion/react";
+import { motion, AnimatePresence } from "motion/react";
 
 export default function CommissionPage() {
   let [step, setStep] = useState(1);
@@ -37,78 +37,96 @@ export default function CommissionPage() {
   return (
     <div className="absolute top-0 -z-10 w-full h-screen flex md:flex-row flex-col">
       <div className="w-full h-full flex justify-center items-center p-3">
-        {step == 1 && (
-          <div className="drop-shadow-2xl">
+        <AnimatePresence>
+          {step == 1 && (
             <motion.div
-              className={clsx(
-                "bg-white rounded-md  flex justify-center items-center scale-75 md:scale-100",
-              )}
-              animate={{
-                width: width + "px",
-                height: height + "px",
-              }}
+              className="drop-shadow-2xl"
+              exit={{ opacity: 0, y: -100 }}
+              initial={{ opacity: 0, y: 100 }}
+              animate={{ opacity: 1, y: 0 }}
             >
-              <span
-                className={clsx("block text-center text-neutral-400 text-xl")}
-              >
-                {width}mm x {height}mm
-              </span>
-            </motion.div>
-          </div>
-        )}
-      </div>
-      <div className="w-full flex justify-center items-center bg-neutral-50 p-3">
-        {step == 1 && (
-          <div className="p-3 gap-3 flex flex-col min-w-96">
-            <h2 className="text-xl">Size</h2>
-            <div className="flex flex-row w-full gap-3 [&>button]:grow">
-              <Button
-                variant={size === "a3" ? "default" : "outline"}
-                onClick={() => setSize("a3")}
-              >
-                A3
-              </Button>
-              <Button
-                variant={size === "a4" ? "default" : "outline"}
-                onClick={() => setSize("a4")}
-              >
-                A4
-              </Button>
-              <Button
-                variant={size === "a5" ? "default" : "outline"}
-                onClick={() => setSize("a5")}
-              >
-                A5
-              </Button>
-            </div>
-            <h2 className="text-xl">Orientation</h2>
-            <div className="flex flex-row w-full gap-3 [&>button]:grow">
-              <Button
-                variant={orientation === "portrait" ? "default" : "outline"}
-                onClick={() => setOrientation("portrait")}
-              >
-                Portrait
-              </Button>
-              <Button
-                variant={orientation === "landscape" ? "default" : "outline"}
-                onClick={() => setOrientation("landscape")}
-              >
-                Landscape
-              </Button>
-            </div>
-            <div className="flex flex-row">
-              <div className="grow"></div>
-              <Button
-                variant="default"
-                onClick={() => {
-                  setStep(2);
+              <motion.div
+                className={clsx(
+                  "bg-white rounded-md  flex justify-center items-center scale-75 md:scale-100",
+                )}
+                initial={{
+                  width: "210px",
+                  height: "297px",
+                }}
+                animate={{
+                  width: width + "px",
+                  height: height + "px",
                 }}
               >
-                Next
-              </Button>
-            </div>
-          </div>
-        )}
+                <span
+                  className={clsx("block text-center text-neutral-400 text-xl")}
+                >
+                  {width}mm x {height}mm
+                </span>
+              </motion.div>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </div>
+      <div className="w-full flex justify-center items-center bg-neutral-50 p-3">
+        <AnimatePresence>
+          {step == 1 && (
+            <motion.div
+              className="p-3 gap-3 flex flex-col min-w-96"
+              exit={{ opacity: 0, y: -100 }}
+              initial={{ opacity: 0, y: 100 }}
+              animate={{ opacity: 1, y: 0 }}
+            >
+              <h2 className="text-xl">Size</h2>
+              <div className="flex flex-row w-full gap-3 [&>button]:grow">
+                <Button
+                  variant={size === "a3" ? "default" : "outline"}
+                  onClick={() => setSize("a3")}
+                >
+                  A3
+                </Button>
+                <Button
+                  variant={size === "a4" ? "default" : "outline"}
+                  onClick={() => setSize("a4")}
+                >
+                  A4
+                </Button>
+                <Button
+                  variant={size === "a5" ? "default" : "outline"}
+                  onClick={() => setSize("a5")}
+                >
+                  A5
+                </Button>
+              </div>
+              <h2 className="text-xl">Orientation</h2>
+              <div className="flex flex-row w-full gap-3 [&>button]:grow">
+                <Button
+                  variant={orientation === "portrait" ? "default" : "outline"}
+                  onClick={() => setOrientation("portrait")}
+                >
+                  Portrait
+                </Button>
+                <Button
+                  variant={orientation === "landscape" ? "default" : "outline"}
+                  onClick={() => setOrientation("landscape")}
+                >
+                  Landscape
+                </Button>
+              </div>
+              <div className="flex flex-row">
+                <div className="grow"></div>
+                <Button
+                  variant="default"
+                  onClick={() => {
+                    setStep(2);
+                  }}
+                >
+                  Next
+                </Button>
+              </div>
+            </motion.div>
+          )}
+        </AnimatePresence>
       </div>
     </div>
   );
